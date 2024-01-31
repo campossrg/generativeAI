6ae63a0a5b3c852c6ab88dd66851aa199cb9f2a5
kempes007 <campos.srg@gmail.com>
2024-01-31 18:50:46 +0100

    document splitters

---
+++ langChain_document_splitter.py
@@ -0,0 +1,60 @@
+import os
+import openai
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.document_loaders import NotionDirectoryLoader
+from langchain.text_splitter import MarkdownHeaderTextSplitter
+
+#openai.api_key  = os.environ['OPENAI_API_KEY']
+
+some_text = """When writing documents, writers will use document structure to group content. \
+This can convey to the reader, which idea's are related. For example, closely related ideas \
+are in sentances. Similar ideas are in paragraphs. Paragraphs form a document. \n\n  \
+Paragraphs are often delimited with a carriage return or two carriage returns. \
+Carriage returns are the "backslash n" you see embedded in this string. \
+Sentences have a period at the end, but also, have a space.\
+and words are separated by space."""
+
+print(len(some_text))
+
+c_splitter = CharacterTextSplitter(
+    chunk_size=450,
+    chunk_overlap=0,
+    separator = ' '
+)
+r_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=450,
+    chunk_overlap=0, 
+    separators=["\n\n", "\n", " ", ""]
+)
+
+print(c_splitter.split_text(some_text))
+print(r_splitter.split_text(some_text))
+print("\n\n")
+
+markdown_document = """# Title\n\n \
+## Chapter 1\n\n \
+Hi this is Jim\n\n Hi this is Joe\n\n \
+### Section \n\n \
+Hi this is Lance \n\n 
+## Chapter 2\n\n \
+Hi this is Molly"""
+
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
+markdown_splitter = MarkdownHeaderTextSplitter(
+    headers_to_split_on=headers_to_split_on
+)
+md_header_splits = markdown_splitter.split_text(markdown_document)
+
+print(md_header_splits[0])
+print(md_header_splits[2])
