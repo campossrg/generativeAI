3f2ffd84d4f45ec9b525d88541c492c532db14d1
kempes007 <campos.srg@gmail.com>
2024-01-31 19:03:58 +0100

    vector examples

---
+++ langChain_document_vectors_embeddings.py
@@ -0,0 +1,51 @@
+import os
+import openai
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv
+load_dotenv()
+
+#openai.api_key  = os.environ['OPENAI_API_KEY']
+
+from langchain_community.document_loaders import PyPDFLoader
+
+# Load PDF
+loaders = [
+    # Duplicate documents on purpose - messy data
+    PyPDFLoader("docs/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/MachineLearning-Lecture02.pdf"),
+    PyPDFLoader("docs/MachineLearning-Lecture03.pdf")
+]
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
+
+# Split
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 150
+)
+
+splits = text_splitter.split_documents(docs)
+
+print(len(splits))
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+embedding = OpenAIEmbeddings()
+
+sentence1 = "i like dogs"
+sentence2 = "i like canines"
+sentence3 = "the weather is ugly outside"
+
+embedding1 = embedding.embed_query(sentence1)
+embedding2 = embedding.embed_query(sentence2)
+embedding3 = embedding.embed_query(sentence3)
+
+import numpy as np
+
+print(np.dot(embedding1, embedding2))
+print(np.dot(embedding1, embedding3))
+print(np.dot(embedding2, embedding3))
