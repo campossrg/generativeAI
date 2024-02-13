547f65ecc93bcf89a1fcde5adf511ce5acd202f3
kempes007 <campos.srg@gmail.com>
2024-02-13 08:54:05 +0100

    full training example

---
+++ lamini_training_process.py
@@ -0,0 +1,234 @@
+# This is an example from DeepLearning.ai as reference
+# Many of the resources can only be executed on their virtual machine
+# as they have specific libraries they don't share
+
+import os
+import lamini
+
+lamini.api_key = ""
+
+import datasets
+import tempfile
+import logging
+import random
+import config
+import os
+import yaml
+import time
+import torch
+import transformers
+import pandas as pd
+import jsonlines
+
+# from utilities import * # what is this for?
+from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM
+from transformers import TrainingArguments
+from transformers import AutoModelForCausalLM
+from llama import BasicModelRunner
+
+
+logger = logging.getLogger(__name__)
+global_config = None
+
+dataset_name = "docs/lamini_docs.jsonl"
+dataset_path = f"/content/{dataset_name}"
+use_hf = False
+
+dataset_path = "lamini/lamini_docs"
+use_hf = True
+
+model_name = "EleutherAI/pythia-70m"
+
+training_config = {
+    "model": {
+        "pretrained_name": model_name,
+        "max_length" : 2048
+    },
+    "datasets": {
+        "use_hf": use_hf,
+        "path": dataset_path
+    },
+    "verbose": True
+}
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
+train_dataset, test_dataset = tokenize_and_split_data(training_config, tokenizer)
+
+print(train_dataset)
+print(test_dataset)
+
+base_model = AutoModelForCausalLM.from_pretrained(model_name)
+
+device_count = torch.cuda.device_count()
+if device_count > 0:
+    logger.debug("Select GPU device")
+    device = torch.device("cuda")
+else:
+    logger.debug("Select CPU device")
+    device = torch.device("cpu")
+
+base_model.to(device)
+
+def inference(text, model, tokenizer, max_input_tokens=1000, max_output_tokens=100):
+  # Tokenize
+  input_ids = tokenizer.encode(
+          text,
+          return_tensors="pt",
+          truncation=True,
+          max_length=max_input_tokens
+  )
+
+  # Generate
+  device = model.device
+  generated_tokens_with_prompt = model.generate(
+    input_ids=input_ids.to(device),
+    max_length=max_output_tokens
+  )
+
+  # Decode
+  generated_text_with_prompt = tokenizer.batch_decode(generated_tokens_with_prompt, skip_special_tokens=True)
+
+  # Strip the prompt
+  generated_text_answer = generated_text_with_prompt[0][len(text):]
+
+  return generated_text_answer
+
+test_text = test_dataset[0]['question']
+print("Question input (test):", test_text)
+print(f"Correct answer from Lamini docs: {test_dataset[0]['answer']}")
+print("Model's answer: ")
+print(inference(test_text, base_model, tokenizer))
+
+max_steps = 3
+
+trained_model_name = f"lamini_docs_{max_steps}_steps"
+output_dir = trained_model_name
+
+training_args = TrainingArguments(
+
+  # Learning rate
+  learning_rate=1.0e-5,
+
+  # Number of training epochs
+  num_train_epochs=1,
+
+  # Max steps to train for (each step is a batch of data)
+  # Overrides num_train_epochs, if not -1
+  max_steps=max_steps,
+
+  # Batch size for training
+  per_device_train_batch_size=1,
+
+  # Directory to save model checkpoints
+  output_dir=output_dir,
+
+  # Other arguments
+  overwrite_output_dir=False, # Overwrite the content of the output directory
+  disable_tqdm=False, # Disable progress bars
+  eval_steps=120, # Number of update steps between two evaluations
+  save_steps=120, # After # steps model is saved
+  warmup_steps=1, # Number of warmup steps for learning rate scheduler
+  per_device_eval_batch_size=1, # Batch size for evaluation
+  evaluation_strategy="steps",
+  logging_strategy="steps",
+  logging_steps=1,
+  optim="adafactor",
+  gradient_accumulation_steps = 4,
+  gradient_checkpointing=False,
+
+  # Parameters for early stopping
+  load_best_model_at_end=True,
+  save_total_limit=1,
+  metric_for_best_model="eval_loss",
+  greater_is_better=False
+)
+
+model_flops = (
+  base_model.floating_point_ops(
+    {
+       "input_ids": torch.zeros(
+           (1, training_config["model"]["max_length"])
+      )
+    }
+  )
+  * training_args.gradient_accumulation_steps
+)
+
+print(base_model)
+print("Memory footprint", base_model.get_memory_footprint() / 1e9, "GB")
+print("Flops", model_flops / 1e9, "GFLOPs")
+
+trainer = Trainer(
+    model=base_model,
+    model_flops=model_flops,
+    total_steps=max_steps,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+)
+
+training_output = trainer.train()
+
+save_dir = f'{output_dir}/final'
+
+trainer.save_model(save_dir)
+print("Saved model to:", save_dir)
+
+finetuned_slightly_model = AutoModelForCausalLM.from_pretrained(save_dir, local_files_only=True)
+
+finetuned_slightly_model.to(device) 
+
+test_question = test_dataset[0]['question']
+print("Question input (test):", test_question)
+
+print("Finetuned slightly model's answer: ")
+print(inference(test_question, finetuned_slightly_model, tokenizer))
+
+test_answer = test_dataset[0]['answer']
+print("Target answer output (test):", test_answer)
+
+finetuned_longer_model = AutoModelForCausalLM.from_pretrained("lamini/lamini_docs_finetuned")
+tokenizer = AutoTokenizer.from_pretrained("lamini/lamini_docs_finetuned")
+
+finetuned_longer_model.to(device)
+print("Finetuned longer model's answer: ")
+print(inference(test_question, finetuned_longer_model, tokenizer))
+
+bigger_finetuned_model = BasicModelRunner(model_name_to_id["bigger_model_name"])
+bigger_finetuned_output = bigger_finetuned_model(test_question)
+print("Bigger (2.8B) finetuned model (test): ", bigger_finetuned_output)
+
+count = 0
+for i in range(len(train_dataset)):
+ if "keep the discussion relevant to Lamini" in train_dataset[i]["answer"]:
+  print(i, train_dataset[i]["question"], train_dataset[i]["answer"])
+  count += 1
+print(count)
+
+# Use the same but with an small model
+base_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-70m")
+base_model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-70m")
+print(inference("What do you think of Mars?", base_model, base_tokenizer))
+
+print(inference("What do you think of Mars?", finetuned_longer_model, tokenizer))
+
+# Use lamini to train with few lines
+model = BasicModelRunner("EleutherAI/pythia-410m") 
+model.load_data_from_jsonlines("lamini_docs.jsonl", input_key="question", output_key="answer")
+model.train(is_public=True) 
+
+out = model.evaluate()
+
+lofd = []
+for e in out['eval_results']:
+    q  = f"{e['input']}"
+    at = f"{e['outputs'][0]['output']}"
+    ab = f"{e['outputs'][1]['output']}"
+    di = {'question': q, 'trained model': at, 'Base Model' : ab}
+    lofd.append(di)
+df = pd.DataFrame.from_dict(lofd)
+style_df = df.style.set_properties(**{'text-align': 'left'})
+style_df = style_df.set_properties(**{"vertical-align": "text-top"})
+style_df
